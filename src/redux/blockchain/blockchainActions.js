4f8a7042b457a204aa0cf786f6f7134b1ca18f7d
BajaBeastMode <102617738+BajaBeastMode@users.noreply.github.com>
2022-03-31 22:03:57 +0100

    Update blockchainActions.js

--- src/redux/blockchain/blockchainActions.js
+++ src/redux/blockchain/blockchainActions.js
@@ -58,9 +58,9 @@ export const connect = () => {
           method: "eth_requestAccounts",
         });
         const networkId = await ethereum.request({
-          method: "test_version",
+          method: "net_version",
         });
-        if (networkId == 80001) {
+        if (networkId == CONFIG.NETWORK.ID) {
           const SmartContractObj = new Web3EthContract(
             abi,
             CONFIG.CONTRACT_ADDRESS
